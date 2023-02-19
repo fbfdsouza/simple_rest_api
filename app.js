4770b812758c5910fe7bc4c3786cb536db8edf3d
Bruno Filgueiras <fbfdsouza@gmail.com>
2023-02-19 16:47:42 -0300

    adding cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const cors = require('cors');
 mongoose.set('strictQuery', true);
 require('dotenv').config();
 const usersRoutes = require('./routes/users');
@@ -11,19 +12,20 @@ const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 
 mongoose.connect(
-    MONGO_DB_URI,
-    (err) => {
-     if(err) console.log(err) 
-     else console.log("mongdb is connected");
-    }
-  );
-  
-  app.use(express.static('./public'));
-  app.use(bodyParser.json());
+  MONGO_DB_URI,
+  (err) => {
+    if (err) console.log(err)
+    else console.log("mongdb is connected");
+  }
+);
 
-  app.use('/api/users', usersRoutes);
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(cors());
 
-  // handler for 404 - Resource Not Found
+app.use('/api/users', usersRoutes);
+
+// handler for 404 - Resource Not Found
 app.use((_, res) => {
   res.status(404).send(`The address doesn't exist`);
 });
